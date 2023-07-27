8290d139b207073e07fe372686800969989ac731
zafar <zafarhussain633@gmail.com>
2023-07-28 02:20:24 +0530

    reverted to unsafe inline

--- pages/_document.js
+++ pages/_document.js
@@ -10,13 +10,12 @@ function MyDocument(props) {
   const { Head } = provideComponents(props);
   return (
     <Html>
-      <Head nonce={nonce}>
+      <Head >
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap"
           rel="stylesheet"
-          nonce={nonce}
         />
        
 
@@ -25,7 +24,7 @@ function MyDocument(props) {
       </Head>
       <body>
         <Main />
-        <NextScript nonce={nonce} />
+        <NextScript  />
       </body>
     </Html>
   );
