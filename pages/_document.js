be833325f9826b09414f1e3c7497a29324f78cfc
zafar <zafarhussain633@gmail.com>
2023-07-28 02:07:37 +0530

    added nonc ein Nextstrip

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,28 @@ import {
   provideComponents,
 } from "@next-safe/middleware/dist/document";
 import Document, { Html, Main, Head, NextScript } from "next/document";
-import {nonce} from "./../middleware"
+import { nonce } from "./../middleware";
 
 function MyDocument(props) {
   const { Head } = provideComponents(props);
-
   return (
     <Html>
-      <Head>
+      <Head nonce={nonce}>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap"
           rel="stylesheet"
           nonce={nonce}
         />
+       
+
+        
+
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
       </body>
     </Html>
   );
